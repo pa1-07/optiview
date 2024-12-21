b9aceb8eae8b48f40b851f1c0a6bab03253c7ae3
Pavan <jellapavan07@gmail.com>
2024-12-21 11:35:05 +0530

    new change

--- client/src/services/apiEndpoints.js
+++ client/src/services/apiEndpoints.js
@@ -1,10 +1,10 @@
-const BASE_URL = "http://localhost:5000/api" || "https://optiview-server.onrender.com/api";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://optiview-server.onrender.com/api";
 
 export const API = {
   ADMIN_LOGIN: `${BASE_URL}/auth/admin/login`,
   CLIENT_SIGNUP: `${BASE_URL}/auth/signup`,
   IMPORT_SHEET: `${BASE_URL}/sheets/import`,
-  GET_ALL_SHEETS: '/sheets/all',
+  GET_ALL_SHEETS: `${BASE_URL}/sheets/all`,
   GET_ALL_DATA: `${BASE_URL}/sheets/allData`,
   GET_PAGINATED_DATA: `${BASE_URL}/sheets/data`,
   EXPORT_SHEET: `${BASE_URL}/sheets/export/:sheetId`,
