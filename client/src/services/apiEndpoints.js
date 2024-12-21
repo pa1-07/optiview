2717bc557fc166c924685404414d0a6bfa294d23
Pavan <jellapavan07@gmail.com>
2024-12-21 09:58:49 +0530

    New changes

--- client/src/services/apiEndpoints.js
+++ client/src/services/apiEndpoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "http://localhost:5000/api" || "https://optiview-server.onrender.com/api";
 
 export const API = {
   ADMIN_LOGIN: `${BASE_URL}/auth/admin/login`,
--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: ['https://optiview-server.onrender.com','http://localhost:3000']  // Replace with your front-end's domain
+      }
+));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
